0d49f814189d436f8bc8c27c917427f36f889883
emccar22 <e.mccarthy2285@gmail.com>
2022-01-09 18:45:52 -0500

    add generate html file

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -34,13 +34,13 @@ const generateEngineerCard = function (engineer) {
     `;
 };
 
-const geterateInternCard = function () {
+const geterateInternCard = function (intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Manager</h4>
+                <h4>Intern</h4>
             </div>
             <div class="card-body">
                 <p>ID: ${intern.id}</p>
@@ -52,3 +52,59 @@ const geterateInternCard = function () {
     `;
 };
 
+generateHtml = (data) => {
+    pageArray = []
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = generateManagerCard(employee);
+            pageArray.push(managerCard);
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineerCard(employee);
+            pageArray.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = geterateInternCard(employee);
+            pageArray.push(internCard);
+        }
+    };
+
+    const employeeCards = pageArray.join('');
+    const generateTeam = generateTeamPage(employeeCards);
+    return generateTeam;
+}
+
+const generateTeamPage = function (employeeCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <header class="w-100 text-center">
+            <h1>Team Profile</h1>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    ${employeeCards}
+                </div>
+            </div>
+        </main> 
+    </body>
+    </html>
+    `
+};
+
+module.exports = generateHtml;
